5acc71d718fc68034e707018574d70bc6f857b6b
Larry Karnowski <larry@hickorywind.org>
2015-03-15 11:15:25 -0400

    Add my own "journal" package.

---
+++ Packages/User/journal.py
@@ -0,0 +1,19 @@
+import sublime, sublime_plugin, os
+from datetime import datetime
+
+class JournalCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    home = os.environ["HOME"]
+    today = datetime.today()
+    placeholder_text = today.strftime("# Journal %Y-%m-%d %H:%M")
+    journal_dir = home + today.strftime("/Dropbox/Documents/Adzerk/journal/%Y-%m %B")
+    new_file = journal_dir + today.strftime("/%d-%H:%M.md")
+
+    if not os.path.exists(journal_dir):
+      os.makedirs(journal_dir)
+
+    f = open(new_file,'w')
+    f.write(placeholder_text + '\n\n')
+    f.close()
+
+    self.view.window().open_file(new_file)
